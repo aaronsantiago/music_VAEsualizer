a883ed39a33abd7f3e0cdf65af79ff4f17f9035f
Aaron Santiago <aaronsantiago+github@gmail.com>
2020-08-28 12:30:42 -0400

    fix incorrect midi tick rate and related offset values

--- process_midi.py
+++ process_midi.py
@@ -51,28 +51,29 @@ class MVAEsMidiProcessor:
 
     print(self.totalTime)
     # If we're past 2 measures, write a new file
-    if self.totalTime - self.writtenUpTo > 9 / (120/60) and self.autoWrite:
-      self.writeCurrentMidi()
+    if self.totalTime - self.writtenUpTo > 8 / (120/60) and self.autoWrite:
+      self.writeCurrentMidi(self.writtenUpTo)
       self.writtenUpTo += 8 / (120/60)
       # self.totalTime = 0
 
-  def writeCurrentMidi(self):
+  def writeCurrentMidi(self, startWritingAt):
     mid2 = mido.MidiFile()
     track = mido.MidiTrack()
     mid2.tracks.append(track)
 
-    startWritingAt = 0
-    playhead = 0
-    for msg in self.currentMsgQueue:
-      playhead += msg.time
-      # if the entirety of the next 8 bar section is available,
-      # record that one instead of the current one.
-      if playhead > startWritingAt + (8 / (120/60)) * 2:
-        startWritingAt += (8 / (120/60))
+    # startWritingAt = 0
+    # playhead = 0
+    # for msg in self.currentMsgQueue:
+    #   playhead += msg.time
+    #   # if the entirety of the next 8 bar section is available,
+    #   # record that one instead of the current one.
+    #   if playhead > startWritingAt + (8 / (120/60)) * 2:
+    #     startWritingAt += (8 / (120/60))
 
     playhead = 0
     started = False
     accumulatedTimeFromBeginningOfRecording = 0
+    current_note_on = False
     for msg in self.currentMsgQueue:
       playhead += msg.time
       write = False
@@ -86,12 +87,25 @@ class MVAEsMidiProcessor:
         # midi file
         if not started:
           msg.time = playhead - startWritingAt
-        tickTime = int(mido.second2tick(msg.time, 400, 500000))
+
+        leave = False
+        # hope the next message is note_off
+        if playhead > startWritingAt + (8 / (120/60)):
+          if not current_note_on: break
+          msg.time -= playhead - (startWritingAt + (8 / (120/60)))
+          leave = True
+        tickTime = int(mido.second2tick(msg.time, 480, 500000))
         track.append(mido.Message(msg.type,
             note=msg.note,
             velocity=msg.velocity,
             time=tickTime))
+        if leave:
+          break
         started = True
+        if msg.type == "note_on":
+          current_note_on = True
+        else:
+          current_note_on = False
 
     mid2.save(self.outputFileBaseName + ('%d.mid' % self.numSongs))
     self.numSongs += 1
